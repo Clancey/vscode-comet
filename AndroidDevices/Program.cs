23a5b7da78dd855662329cbe17f66d897111811d
Jonathan Dick <jondick@gmail.com>
2020-07-14 09:40:24 -0400

    Remove AndroidSdk dependency

--- AndroidDevices/Program.cs
+++ AndroidDevices/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using AndroidSdk;
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
@@ -7,65 +6,33 @@ using System.IO;
 namespace AndroidDevices
 {
 
-	class DeviceData
-	{
-		public string Name { get; set; }
-		public string Serial { get; set; }
-		public bool IsEmulator { get; set; }
-		public bool IsRunning { get; set; }
-	}
+	
 
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
 			var outputPath = args.FirstOrDefault();
-			if(string.IsNullOrWhiteSpace(outputPath)) {
-				Console.WriteLine("Please pass in the output path");
+
+			var sdkHome = AndroidSdk.FindHome();
+
+			if (sdkHome == null)
+			{
+				Console.WriteLine("Android SDK Not Found");
 				return;
 			}
-			// Make sure all of the tools we need are created and installed
-			var sdk = new AndroidSdkManager();
 
-			// Ensure all the SDK components are installed
-			sdk.Acquire();
+			var devices = AndroidSdk.GetEmulatorsAndDevices(sdkHome);
 
-			var devices = sdk.Adb.GetDevices();
+			foreach(var device in devices)
+				Console.WriteLine($"{device.Name} - {device.IsRunning} - {device.Serial}");
 
-			var emulators = sdk.AvdManager.ListAvds();
-			//var emulators = sdk.Emulator.ListAvds();
-			var foundDevices = new List<DeviceData>();
-			var deviceSerials = new Dictionary<string, string>();
+			if (!string.IsNullOrEmpty(outputPath))
+			{
+				var json = Newtonsoft.Json.JsonConvert.SerializeObject(devices);
 
-			foreach(var device in devices) {
-				var name = sdk.Adb.GetDeviceName(device.Serial);
-				if (device.IsEmulator)
-					deviceSerials[name] = device.Serial;
-				else
-					foundDevices.Add(new DeviceData {
-						IsRunning = true,
-						Name = name,
-						Serial = device.Serial,
-					});
+				File.WriteAllText(outputPath, json);
 			}
-			foreach(var emulator in emulators) {
-				var isRunning = deviceSerials.TryGetValue(emulator.Name, out var serial);
-				foundDevices.Add(new DeviceData {
-					IsEmulator = true,
-					IsRunning = isRunning,
-					Serial = serial,
-					Name = emulator.Name,
-				});
-			}
-
-			//foreach(var device in foundDevices) {
-			//	Console.WriteLine($"{device.Name} - {device.IsRunning} - {device.Serial}");
-			//}
-
-			var json = Newtonsoft.Json.JsonConvert.SerializeObject(foundDevices);
-
-			File.WriteAllText(outputPath, json);
-
 		}
 	}
 }
