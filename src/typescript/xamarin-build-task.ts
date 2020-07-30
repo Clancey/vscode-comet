6f5c591c01eb7a1dbc7ca57b31b3f7e03b0bc4a6
Max Brister <mabriste@microsoft.com>
2020-07-30 14:40:38 -0700

    Resolve msbuild using vswhere on windows

--- src/typescript/xamarin-build-task.ts
+++ src/typescript/xamarin-build-task.ts
@@ -4,6 +4,29 @@ let path = require('path')
 import * as vscode from 'vscode';
 //import {ProjectType } from './msbuild-project-analyzer';
 import { XamarinProjectManager, ProjectType } from './xamarin-project-manager';
+import * as child from 'child_process';
+
+interface ExecResult {
+    stdout: string,
+    stderr: string
+}
+
+function execFileAsync(file: string, args?: string[]): Thenable<ExecResult> {
+
+    return new Promise((resolve, reject) => {
+        child.execFile(file, args, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+            }
+            resolve({ stdout, stderr });
+        });
+    });
+}
+
+function existsAsync(path: string | Buffer): Promise<boolean> {
+
+    return new Promise((resolve) => fs.exists(path, resolve));
+}
 
 interface XamarinBuildTaskDefinition extends vscode.TaskDefinition {
 	/**
@@ -33,7 +56,7 @@ export class XamarinBuildTaskProvider implements vscode.TaskProvider {
 	private configuration:string;
 	private platform:string;
 	
-	// We use a CustomExecution task when state needs to be shared accross runs of the task or when 
+	// We use a CustomExecution task when state needs to be shared across runs of the task or when 
 	// the task requires use of some VS Code API to run.
 	// If you don't need to share state between runs and if you don't need to execute VS Code API in your task, 
 	// then a simple ShellExecution or ProcessExecution should be enough.
@@ -49,12 +72,53 @@ export class XamarinBuildTaskProvider implements vscode.TaskProvider {
 	}
 
 	public resolveTask(_task: vscode.Task): vscode.Task | undefined {
-		return this.getTask(XamarinBuildTaskProvider.msBuildCommand,"Build",[]);
+		// All supported tasks are provided by getTasks. If that changes, the easiest
+		// thing to do might be to move msbuild location into util, and launch util directly.
+		return undefined;
 	}
 
-	static msBuildCommand:string = "msbuild";
+	static msBuildPromise:Promise<string>;
 
-	private getTasks(): vscode.Task[] {
+	private static locateMSBuild(): Promise<string> {
+
+		if (!XamarinBuildTaskProvider.msBuildPromise) {
+			XamarinBuildTaskProvider.msBuildPromise = XamarinBuildTaskProvider.locateMSBuildImpl();
+		}
+		return XamarinBuildTaskProvider.msBuildPromise;
+	}
+
+	private static async locateMSBuildImpl(): Promise<string> {
+
+		if (process.platform !== "win32") {
+			return "msbuild";
+		}
+	
+		// msbuild isn't normally in the path on windows. Use vswhere to locate it.
+		// https://github.com/microsoft/vswhere
+		const progFiles = process.env["ProgramFiles(x86)"];
+		const vswhere = path.join(progFiles, "Microsoft Visual Studio", "Installer", "vswhere.exe");
+		if (!await existsAsync(vswhere)) {
+			return "msbuild";
+		}
+	
+		var findMSBuild = ["-latest", "-requires", "Microsoft.Component.MSBuild", "-find", "MSBuild\\**\\Bin\\MSBuild.exe"];
+		var { stdout } = await execFileAsync(vswhere, findMSBuild);
+		stdout = stdout.trim();
+		if (stdout.length > 0) {
+			return stdout;
+		}
+	
+		findMSBuild.push("-preRelease");
+		var { stdout } = await execFileAsync(vswhere, findMSBuild);
+		stdout = stdout.trim();
+		if (stdout.length > 0) {
+			return stdout;
+		} else {
+			return "msbuild";
+		}
+	}
+
+	private async getTasks(): Promise<vscode.Task[]> {
 		
 		if (!XamarinProjectManager.SelectedProject)
 		{
@@ -68,8 +132,9 @@ export class XamarinBuildTaskProvider implements vscode.TaskProvider {
 
 		var flags = [];
 		var target = "Build";
+		var command = await XamarinBuildTaskProvider.locateMSBuild();
 
-		return [ this.getTask(XamarinBuildTaskProvider.msBuildCommand,target,flags) ]
+		return [ this.getTask(command,target,flags) ]
 	}
 
 	private getTask(command:string ,target: string, flags: string[], definition?: XamarinBuildTaskDefinition): vscode.Task{
@@ -95,8 +160,9 @@ export class XamarinBuildTaskProvider implements vscode.TaskProvider {
 		if (this.platform)
 			platformArg = `;Platform=${platform}`;
 
-		var fullCommand = `${command} ${csproj} /t:${target} /p:Configuration=${configuration}${platformArg} ${flags.join(' ')}`;
-		var task = new vscode.Task(definition, definition.target, 'xamarin', new vscode.ShellExecution(fullCommand));
+		var args = [csproj, `/t:${target}`, `/p:Configuration=${configuration}${platformArg}`];
+		args.concat(flags);
+		var task = new vscode.Task(definition, definition.target, 'xamarin', new vscode.ProcessExecution(command, args));
 		return task;
 	}
 }
