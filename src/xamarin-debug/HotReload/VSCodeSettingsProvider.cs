717cc2f522bad5ea31c347cf1f89c6505d2f5702
Jonathan Dick <jondick@gmail.com>
2020-07-29 22:14:32 -0400

    Revert "Revert "Merge pull request #9 from joshspicer/add-hot-reload""
    This reverts commit 06e8e8c12fca2651055cce0c2754b8d24e3c6332.

---
+++ src/xamarin-debug/HotReload/VSCodeSettingsProvider.cs
@@ -0,0 +1,29 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using Xamarin.HotReload;
+
+namespace VSCodeDebug.HotReload
+{
+	public class VSCodeSettingsProvider : ISettingsProvider
+	{
+		public event EventHandler<SettingsChangedEventArgs> SettingsChanged;
+
+		public bool GetBool(string key, bool defaultValue)
+		{
+			return defaultValue;
+		}
+
+		public void SetBool(string key, bool value)
+		{
+			OnSettingsChanged(key, value);
+		}
+
+		public void OnSettingsChanged(string key, object newValue)
+		{
+			SettingsChanged?.Invoke(this, new SettingsChangedEventArgs(key, newValue));
+		}
+	}
+}
--- src/xamarin-debug/MonoDebugSession.cs
+++ src/xamarin-debug/MonoDebugSession.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Net;
 using VsCodeXamarinUtil;
 using Mono.Debugging.Client;
+using VSCodeDebug.HotReload;
 using System.Threading.Tasks;
 
 namespace VSCodeDebug
@@ -48,6 +49,7 @@ namespace VSCodeDebug
 		private bool _terminated = false;
 		private bool _stderrEOF = true;
 		private bool _stdoutEOF = true;
+		private HotReloadManager _hotReloadManager;
 
 
 		public MonoDebugSession() : base()
@@ -113,6 +115,8 @@ namespace VSCodeDebug
 
 			_session.TargetReady += (sender, e) => {
 				_activeProcess = _session.GetProcesses().SingleOrDefault();
+				_hotReloadManager = new HotReloadManager();
+				_hotReloadManager.StartHR(_session);
 			};
 
 			_session.TargetExited += (sender, e) => {
@@ -151,6 +155,20 @@ namespace VSCodeDebug
 
 			_session.OutputWriter = (isStdErr, text) => {
 				SendOutput(isStdErr ? "stderr" : "stdout", text);
+			};
+
+			this.HandleUnknownRequest = (s) => {
+				if (s.command == "DocumentChanged")
+				{
+					if (_hotReloadManager == null)
+						return false;
+
+					string fullPath = s.args.fullPath;
+					string relativePath = s.args.relativePath;
+					_hotReloadManager.DocumentChanged(fullPath, relativePath);
+					return true;
+				}
+				return false;
 			};
 		}
 
@@ -549,6 +567,10 @@ namespace VSCodeDebug
 
 		public override void StackTrace(Response response, dynamic args)
 		{
+			// TODO: Getting a stack trace can hang; we need to fix it but for now just return an empty one
+			SendResponse(response, new StackTraceResponseBody(new List<StackFrame>(), 0));
+			return;
+
 			int maxLevels = getInt(args, "levels", 10);
 			int threadReference = getInt(args, "threadId", 0);
 
@@ -611,6 +633,12 @@ namespace VSCodeDebug
 			var frame = _frameHandles.Get(frameId, null);
 
 			var scopes = new List<Scope>();
+
+			// TODO: I'm not sure if this is the best response in this scenario but it at least avoids an NRE
+			if (frame == null) {
+				SendResponse(response, new ScopesResponseBody(scopes));
+				return;
+			}
 
 			if (frame.Index == 0 && _exception != null) {
 				scopes.Add(new Scope("Exception", _variableHandles.Create(new ObjectValue[] { _exception })));
@@ -823,6 +851,10 @@ namespace VSCodeDebug
 		private Variable CreateVariable(ObjectValue v)
 		{
 			var dv = v.DisplayValue;
+			if (dv == null) {
+				dv = "<error getting value>";
+			}
+
 			if (dv.Length > 1 && dv [0] == '{' && dv [dv.Length - 1] == '}') {
 				dv = dv.Substring (1, dv.Length - 2);
 			}
