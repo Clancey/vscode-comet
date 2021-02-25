3fbef3a92f92ea12f8a98c114c6ee482d11a1d6b
Jonathan Dick <jondick@gmail.com>
2021-02-25 09:19:09 -0500

    Remove create project bits, dotnet new is the right way forward

--- src/typescript/xamarin-commands.ts
+++ src/typescript/xamarin-commands.ts
@@ -6,64 +6,6 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 // Initiates the flow of creating a new project.  Is opinionated.
-export async function newProject() {
-
-    var dialogOpts: vscode.OpenDialogOptions = {
-        canSelectFiles: false,
-        canSelectFolders: true,
-        openLabel: "Create Xamarin Project"
-    };
-    var folderURI: vscode.Uri[] = await vscode.window.showOpenDialog(dialogOpts);
-    var folder: vscode.Uri = folderURI[0];
-
-    var templateName: string = "forms-app";
-    var templateKinds = ["blank", "master-detail", "tabbed", "shell"];
-
-    var quickpicks = await vscode.window.showQuickPick(templateKinds, {
-        canPickMany: false,
-        placeHolder: "Choose a Xamarin Forms template"
-    });
-    var templateKind: string = quickpicks[0];
-
-    vscode.window.showInformationMessage(`Creating project....`);
-
-    await createProject(templateName, templateKind, folder);
-
-    var projectPath: string = folder.fsPath;
-    var projectName: string = projectPath.substring(projectPath.lastIndexOf('/') + 1);
-
-    var ioscsproj = path.join(projectPath, `${projectName}.iOS`, `${projectName}.iOS.csproj`);
-    var androidcsproj = path.join(projectPath, `${projectName}.Android`, `${projectName}.Android.csproj`);
-
-    console.log("ios" + ioscsproj);
-    console.log("android" + androidcsproj);
-
-    // Add correct project references
-    await rewriteProjectName(projectName, androidcsproj);
-    await rewriteProjectName(projectName, ioscsproj);
-    
-}   
-
-async function rewriteProjectName(projectName: string, pathToCsProj: string) {
-
-    fs.readFile(pathToCsProj, 'utf8', function (err,data) {
-        if (err) {
-            console.log(`${err.name} ${err.message}`);
-            vscode.window.showErrorMessage(`${err.name} ${err.message}`);
-            
-            return;
-        }
-        var result = data.replace(/LibraryProjectName/g, projectName);
-      
-        fs.writeFile(pathToCsProj, result, 'utf8', function (err) {
-           if (err) {
-            console.log(`${err.name} ${err.message}`);
-            vscode.window.showErrorMessage(`${err.name} ${err.message}`);
-            return;
-           }
-        });
-      });
-}
 
 function execFileAsync(file: string, args?: string[]): Thenable<Error> {
     
@@ -72,33 +14,6 @@ function execFileAsync(file: string, args?: string[]): Thenable<Error> {
             resolve(error);
         });
     });
-}
-
-async function createProject(templateName: string, templateKind: string, folder: vscode.Uri) {
-
-    const createProject = ["new", templateName, "-o", folder.fsPath, "-k", templateKind];
-    var extPath = vscode.extensions.getExtension('ms-vscode.xamarin').extensionPath;
-    const installTemplates = ["new", "--install", path.join(extPath, "templates", "Xamarin.Templates.Multiplatform.0.0.1.nupkg")];
-
-    var error = await execFileAsync("dotnet", createProject);
-    if (error) {
-        error = await execFileAsync("dotnet", installTemplates);
-        if (!error) {
-            error = await execFileAsync("dotnet", createProject);
-        }
-    }
-
-    if (error) {
-        var message = error.message;
-        if ((error as any).code == "ENOENT") {
-            // Normally the error message is very informative. If dotnet can't be located the error message needs fixing.
-            message = "Error! Make sure you have the dotnet cli tool installed.";
-        }
-        vscode.window.showErrorMessage(message);
-    } else {
-        vscode.window.showInformationMessage(`Created a ${templateKind}!`);
-        await vscode.commands.executeCommand("vscode.openFolder", folder);
-    }
 }
 
 
