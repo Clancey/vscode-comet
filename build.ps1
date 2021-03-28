fe138cf52c1c645afcd538c73d5b0fd8dad5fa0a
Jonathan Dick <jond@hey.com>
2021-03-28 13:32:34 -0400

    Cleanup

--- build.ps1
+++ build.ps1
@@ -6,8 +6,6 @@ if (-Not $cmd)
 	$cmd = "debug"
 }
 
-$ExcludeHotReload = (Test-Path -Path .\exclude-hot-reload.txt)
-
 function Vsix
 {
 	Write-Host "Creating VSIX..."
@@ -16,15 +14,15 @@ function Vsix
 }
 function BuildNet
 {
-	Write-Host "Building .NET Project...(ExcludeHotReload: $ExcludeHotReload)"
+	Write-Host "Building .NET Project..."
 
-	& msbuild /r /p:Configuration=Debug /p:nugetInteractive=true /p:ExcludeHotReload=$ExcludeHotReload ./src/xamarin-debug/xamarin-debug.csproj
+	& msbuild /r /p:Configuration=Debug ./src/xamarin-debug/xamarin-debug.csproj
 
-	Write-Host "Done .NET Project (ExcludeHotReload: $ExcludeHotReload)."
+	Write-Host "Done .NET Project."
 
 	Write-Host "Building Reloadify 3000 "
 
-	& dotnet build /p:Configuration=Debug /p:nugetInteractive=true ./external/Reloadify3000/Reloadify.CommandLine/Reloadify.CommandLine.csproj
+	& dotnet build /p:Configuration=Debug ./external/Reloadify3000/Reloadify.CommandLine/Reloadify.CommandLine.csproj
 
 	Write-Host "Copying Reloadify 3000 output"
 	& Copy-Item  ./external/Reloadify3000/Reloadify.CommandLine/bin/debug/net6.0/*  ./src/xamarin-debug/bin/debug/net472/Reloadify -Recurse -force
