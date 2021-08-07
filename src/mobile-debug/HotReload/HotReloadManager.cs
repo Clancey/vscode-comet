74bc8659026579da57758bfee28b35e720079c46
James Clancey <james.clancey@gmail.com>
2021-08-07 11:38:36 -0800

    Fixed the build

--- src/mobile-debug/HotReload/HotReloadManager.cs
+++ src/mobile-debug/HotReload/HotReloadManager.cs
@@ -36,7 +36,7 @@ namespace VSCodeDebug.HotReload
 			args.AppendQuoted(reloadifyPath);
 
 			args.AppendQuoted(launchData.Project);
-			args.Append($"-t={launchOptions.ProjectTargetFramework}");
+			args.Append($"-t={launchData.ProjectTargetFramework}");
 			args.Append($"-p={launchData.Platform}");
 			args.Append($"-c={launchData.Configuration}");
 			args.Append($"-f=\"{launchData.WorkspaceDirectory}\"");
