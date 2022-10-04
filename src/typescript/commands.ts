603fcd45f0ffea15c7565b973a7ab398e18469fe
redth <jondick@gmail.com>
2022-10-03 22:26:17 -0400

    Cleanup

--- src/typescript/commands.ts
+++ src/typescript/commands.ts
@@ -1,9 +1,6 @@
 import * as vscode from 'vscode';
 import { EmulatorItem, MobileEmulatorProvider } from "./sidebar"
-import { create } from 'lodash';
 import * as child from 'child_process';
-import * as path from 'path';
-import * as fs from 'fs';
 
 // Initiates the flow of creating a new project.  Is opinionated.
 
