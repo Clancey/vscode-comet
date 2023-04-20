f0eb006a1fc4b8e534937398d69c5bd96233cb0a
Dominique Louis <savagesoftware@gmail.com>
2023-04-20 14:47:32 +0100

    Add ability to run release configuration.

--- src/typescript/extensionInfo.ts
+++ src/typescript/extensionInfo.ts
@@ -2,7 +2,6 @@ import * as vscode from 'vscode';
 const path = require('path');
 
 export const dotnetVersion = "net7.0";
-export const dotnetConfiguration = "Debug";
 export const mobileBuildScriptType = "comet";
 export const omnisharpExtensionId = "ms-dotnettools.csharp";
 export const extensionConfigurationKey = "comet-debug";
@@ -11,5 +10,8 @@ export const outputChanelName = "Comet for .NET Mobile";
 export const extensionPath = vscode.extensions.getExtension(extensionId).extensionPath;
 export const configureExtensionCommand = "extension.comet.configureExceptions";
 export const startSessionCommand = "extension.comet.startSession";
+
+const isProduction = process.env.NODE_ENV === "production";
+const dotnetConfiguration = isProduction ? "Release" : "Debug";
 export const analyzerExePath = path.join(extensionPath, 'src', 'DotNetWorkspaceAnalyzer', 'bin', dotnetConfiguration, dotnetVersion, 'DotNetWorkspaceAnalyzer.dll');
 export const mobileDebugPath = path.join(extensionPath, 'src', 'mobile-debug', 'bin', dotnetConfiguration, dotnetVersion, 'mobile-debug.dll');
